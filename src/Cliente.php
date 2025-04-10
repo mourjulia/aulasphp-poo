ebbc669186765bcb60ae87cfc962278786593530
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-04-10 08:58:00 -0300

    criando cliente.php

---
+++ src/Cliente.php
@@ -0,0 +1,19 @@
+<?php
+class Cliente
+{
+    //Atributos (características da Classe)
+    public string $nome;
+    public int $idade;
+    public string $email;
+ 
+    //Métodos (comportamentos da Classe)
+    function exibirDados(): void
+    {
+        echo "Este é o cliente... $this->nome";
+    }
+ 
+    public function verificarIdade(int $idade): bool
+    {
+        return true;
+    }
+}
