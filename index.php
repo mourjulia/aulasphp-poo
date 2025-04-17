e0a3a1a770c9f1ead61754e6f4e8dc20e662f8ba
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-04-17 08:25:09 -0300

    adicionando novo material na branch 03

--- index.php
+++ index.php
@@ -3,50 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 02</title>
+    <title>Exemplo 03</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 02</h1>
+    <h1>PHP com POO - Exemplo 03</h1>
     <hr>
- 
+
 <?php
-// Carregando/importando a classe
 require_once "src/Cliente.php";
- 
-// Criando objeto/instância da Classe
-$clienteA = new Cliente ();
-$clienteB = new Cliente ();
- 
-//Atribuindo valores oara atributos
-$clienteA->setNome("Alana Silva Rocha");
-$clienteA->setIdade(17);
-$clienteA->setEmail( "alana@gmail.com");
- 
-$clienteB->setNome("Clara Ruiz Igreja");
-$clienteB->setIdade(16);
-$clienteB->setEmail("clara@gmail.com");
+
+$clienteA = new Cliente("Fulano", 30, "fulano@gmail.com");
+$clienteB = new Cliente("Ozzy", 15, "ozzy@gmail.com");
 ?>
- 
+
 <h2>Acessando/lendo os dados dos objetos</h2>
- 
 
 <ul>
-    <li><b>E-mail:</b> <?=$clienteB->getNome()?></li>
-    <li><b>Idade:</b> <?=$clienteA->getIdade()?></li>
-    <li><b>E-mail:</b> <?=$clienteA->getEmail()?></li>
+    <li><b>Nome:</b> <?=$clienteA->getNome()?> </li>
+    <li><b>Idade:</b> <?=$clienteA->getIdade()?> </li>
+    <li><b>E-mail:</b> <?=$clienteA->getEmail()?> </li>
+    <li><b>Faixa etária:</b> <?=$clienteA->verificarIdade()?> </li>
 </ul>
- 
 
 <ul>
-    <li><b>E-mail:</b> <?=$clienteB->getNome()?></li>
-    <li><b>Idade:</b> <?=$clienteB->getIdade()?></li>
-    <li><b>E-mail:</b> <?=$clienteB->getEmail()?></li>
+    <li><b>Nome:</b> <?=$clienteB->getNome()?> </li>
+    <li><b>Idade:</b> <?=$clienteB->getIdade()?> </li>
+    <li><b>E-mail:</b> <?=$clienteB->getEmail()?> </li>
+    <li><b>Faixa etária:</b> <?=$clienteB->verificarIdade()?> </li>
 </ul>
- 
+
+
 <h2>Testes de acesso aos recursos da classe através do objeto</h2>
- 
+
 <pre><?=var_dump($clienteA, $clienteB)?></pre>
- 
- 
+
+
 </body>
 </html>
--- src/Cliente.php
+++ src/Cliente.php
@@ -5,13 +5,26 @@ class Cliente
     private int $idade;
     private string $email;
 
-    // metodos setters: responsavel por receber/atribuir dados para as propriedade/atributos
-    public function setNome(string $nome): void
+    // Método Construtor (sempre é executado automaticamente ao criar objeto)
+    public function __construct(string $nome, int $idade, string $email)
+    {
+        $this->setNome($nome);
+        $this->setIdade($idade);
+        $this->setEmail($email);
+    }
+
+    public function verificarIdade(): string {
+        if( $this->idade < 18 ) return "menor de idade";
+        if( $this->idade < 60 ) return "adulto";
+        return "idoso";
+    }
+
+    private function setNome(string $nome): void 
     {
         $this->nome = $nome;
     }
 
-    public function setIdade(int $idade): void
+    private function setIdade(int $idade): void 
     {
         if($idade < 0){
             throw new InvalidArgumentException("Idade não pode ser negativa");
@@ -20,29 +33,28 @@ class Cliente
         $this->idade = $idade;
     }
 
-    public function setEmail(string $email): void
+    private function setEmail(string $email): void 
     {
         if( !filter_var($email, FILTER_VALIDATE_EMAIL) ){
             throw new InvalidArgumentException("E-mail inválido!");
         }
+
         $this->email = $email;
     }
 
-    // metodos getters: responsavel por acessar/ler os dados
-    public function getNome():string
+    public function getNome(): string 
     {
-        return  $this->nome;
+        return $this->nome;
     }
 
-    public function getIdade():int
+    public function getIdade(): int
     {
-        return  $this->idade;
+        return $this->idade;
     }
 
-    public function getEmail():string
+    public function getEmail(): string 
     {
-        return  $this->email;
+        return $this->email;
     }
-
 
 }
