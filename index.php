33daf1b8e6133f186247c4d94154de508c2ce742
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-04-17 08:30:59 -0300

    Adicionando novos materiais a branch 04

--- index.php
+++ index.php
@@ -3,39 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 03</title>
+    <title>Exemplo 04</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 03</h1>
+    <h1>PHP com POO - Exemplo 04</h1>
     <hr>
 
 <?php
-require_once "src/Cliente.php";
+require_once "src/Cliente.php"; // Superclasse
+require_once "src/PessoaFisica.php"; // Subclasse
 
-$clienteA = new Cliente("Fulano", 30, "fulano@gmail.com");
-$clienteB = new Cliente("Ozzy", 15, "ozzy@gmail.com");
+$clientePF = new PessoaFisica("Tiago", "tiago@gmail.com", 30, "123.456.789-00");
 ?>
 
-<h2>Acessando/lendo os dados dos objetos</h2>
-
-<ul>
-    <li><b>Nome:</b> <?=$clienteA->getNome()?> </li>
-    <li><b>Idade:</b> <?=$clienteA->getIdade()?> </li>
-    <li><b>E-mail:</b> <?=$clienteA->getEmail()?> </li>
-    <li><b>Faixa etária:</b> <?=$clienteA->verificarIdade()?> </li>
-</ul>
-
-<ul>
-    <li><b>Nome:</b> <?=$clienteB->getNome()?> </li>
-    <li><b>Idade:</b> <?=$clienteB->getIdade()?> </li>
-    <li><b>E-mail:</b> <?=$clienteB->getEmail()?> </li>
-    <li><b>Faixa etária:</b> <?=$clienteB->verificarIdade()?> </li>
-</ul>
-
-
-<h2>Testes de acesso aos recursos da classe através do objeto</h2>
-
-<pre><?=var_dump($clienteA, $clienteB)?></pre>
+<pre><?=var_dump($clientePF)?></pre>
 
 
 </body>
